7399baf1e856b1e9d4cb831248d2b7b8445a6c99
jermu01 <49148862+jermu01@users.noreply.github.com>
2020-04-08 14:06:35 +0300

    Add files via upload

---
+++ JS-Teemat/03_arrays/array.js
@@ -0,0 +1,12 @@
+let car = `volvo`;
+let car2 = `BMW`;
+
+let cars = new Array(`volvo`,`BMW`,`Ford`);
+let fruits = [`apples`,`oranges`,`pears`];
+
+console.log(car[1]);
+cars[3] = `Fiat`;
+console.log(cars);
+console.log(typeof cars);
+
+console.log(fruits.length);
